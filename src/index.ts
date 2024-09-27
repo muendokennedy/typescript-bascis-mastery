f8dd89f5d2875528ef72ca0d4e3a11498f1c72c7
muendokennedy <kennedymunyao999@gmai.com>
2024-09-27 12:50:56 +0300

    Made a function that receives the union types as parameters or arguments

--- src/index.ts
+++ src/index.ts
@@ -47,6 +47,16 @@ function completeOrder(orderId: number){
   return order
 }
 
+function getPizzaDetail(identifier: string | number): Pizza | undefined{
+  if(typeof identifier === 'string'){
+    return menu.find(pizza => pizza.name.toLowerCase() === identifier.toLowerCase())
+  } else if(typeof identifier === 'number') {
+    return menu.find(pizza => pizza.id === identifier)
+  } else {
+    throw new TypeError('Parameter `identifier` must be either a string or a number')
+  }
+}
+
 addNewPizza({id: 5, name: 'Chicken Bacon Ranch', price: 12})
 addNewPizza({id: 6, name: 'BBQ Chicken', price: 12})
 addNewPizza({id: 7, name: 'Spicy Sausage', price: 8})
